f34c9162fe324a69b6629f9b17b96ba638983789
unknown <Administrateur@102SE22-BMQJVN2.ad.campus-eni.fr>
2021-08-13 14:32:55 +0200

    Etape 8 ajout personne Ok !

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Equipe;
+use App\Entity\Personne;
 use App\Form\EquipeType;
+use App\Form\PersonneType;
 use App\Repository\EquipeRepository;
+use App\Repository\PersonneRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,13 +19,20 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(EquipeRepository $repoEquipe): Response
+    public function index(EquipeRepository $repoEquipe, PersonneRepository $repoPersonne): Response
     {
         $equipe = new Equipe();
         $equipeForm = $this->createForm(EquipeType::class, $equipe);
+
+        $personne = new Personne();
+        $personneForm = $this->createForm(PersonneType::class, $personne);
+
         return $this->render('index.html.twig', [
             'equipeForm' => $equipeForm->createView(),
+            'personneForm' => $personneForm->createView(),
+
             'equipes' => $repoEquipe->findAll(),
+            'personnes' => $repoPersonne->findAll(),
         ]);
     }
     /**
@@ -37,6 +47,26 @@ class MainController extends AbstractController
         $equipeForm->handleRequest($request);
         if ($equipeForm->isSubmitted() && $equipeForm->isValid()) {
             $em->persist($equipe);
+            $em->flush();
+        }
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/ajouter/personne/", name="personne_ajouter")
+     */
+    public function ajouterPersonne(Request $request, EntityManagerInterface $em): Response
+    {
+        //dd('route ajouter equipe');
+        ///$em = $this->getDoctrine()->getManager();
+        $personne = new Personne();
+        $personneForm = $this->createForm(PersonneType::class, $personne);
+        $personneForm->handleRequest($request);
+        if ($personneForm->isSubmitted() && $personneForm->isValid()) {
+            $equipe = $personneForm->get('equipes')->getData();
+            if ($equipe)
+                $personne->addEquipe($equipe);
+            $em->persist($personne);
             $em->flush();
         }
         return $this->redirectToRoute('home');
--- src/Form/PersonneType.php
+++ src/Form/PersonneType.php
@@ -2,25 +2,29 @@
 
 namespace App\Form;
 
-use App\Entity\Equipe;
+use App\Entity\Personne;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EquipeType extends AbstractType
+class PersonneType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom', null, ['attr' => ['placeholder' => 'Equipe ']]);
-            //->add('personnes')
-        ;
+            ->add('prenom')
+            ->add('nom')
+            ->add('equipes', null, [
+                'choice_label' => 'nom',
+                'mapped' => false,
+                'multiple' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Equipe::class,
+            'data_class' => Personne::class,
         ]);
     }
 }
